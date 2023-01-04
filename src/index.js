127eba17bef08a2bf58be3cb1e36a0d4d46ff764
Faizan Ullah <faizanullah1999@gmail.com>
2023-01-04 23:50:55 +0500

    Fixed Image not viewing bug

--- src/index.js
+++ src/index.js
@@ -1,38 +1,52 @@
-const express = require("express")
-const path = require("path")
-const hbs = require("hbs")
-const collection = require("./mongodb")
-require('dotenv').config({ path: __dirname + '/.env' })
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 
+const PORT = 3000;
 const app = express();
-const templatePath = path.join(__dirname, '../templates')
+const templatePath = path.join(__dirname, "../templates");
 
-app.use(express.json())
-app.set("view engine", "hbs")
-app.set("views", templatePath)
-app.use(express.urlencoded({ extended: false }))
+//MODELS
+const collection = require("./models/user")
+
+
 app.use(express.json());
-
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("login")
-})
+    res.render("login");
+});
 
 app.get("/signup", (req, res) => {
-    res.render("signup")
-})
+    res.render("signup");
+});
 
 app.post("/signup", async(req, res) => {
     const data = {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-    }
-    await collection.insertMany([data])
-    res.render("login")
-})
+        password: req.body.password,
+    };
+    await collection.insertMany([data]);
+    res.render("login");
+});
 
 
-app.listen(3000, () => {
-    console.log("Port Connected")
-})
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log("mongodb connected");
+        const server = app.listen(PORT, () => {
+            console.log(`Server Started 💚 at http://localhost:3000`);
+        });
+    })
+    .catch((error) => {
+        console.log("Failed to connect to MongoDB.\n", { error });
+    });
